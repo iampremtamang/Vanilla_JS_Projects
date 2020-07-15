b42e026afb13814a87a9cb79d01f60aa475a582c
Prem-K-Tamang <ktamangprem@gmail.com>
2020-07-15 14:13:54 +0545

    Reviews completed

--- reviews/app.js
+++ reviews/app.js
@@ -44,43 +44,47 @@ const job = document.getElementById("job");
 const info = document.getElementById("info");
 
 const prevBtn = document.querySelector(".prev-btn");
-const nextBtn =  document.querySelector(".next-btn");
+const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
 // set starting item
 let currentItem = 0;
 
-
 // load initial item
 window.addEventListener("DOMContentLoaded", () => {
-    showPerson(currentItem);
-  
+  showPerson(currentItem);
 });
-
 
 // show person based on item
 function showPerson(person) {
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 }
 
 // show next person
 
-nextBtn.addEventListener("click", ()=> {
-    currentItem++;
-    if (currentItem >= reviews.length) {
-      currentItem = 0;
-    }
-    showPerson(currentItem);
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if (currentItem >= reviews.length) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
 });
 
-prevBtn.addEventListener("click", ()=> {
-    currentItem--;
-    if (currentItem < 0) {
-        currentItem = 3;
-    }
-    showPerson(currentItem);
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = 3;
+  }
+  showPerson(currentItem);
+});
+
+
+randomBtn.addEventListener("click",()=> {
+  let currentItem = Math.floor(Math.random()*reviews.length);
+  console.log(currentItem);
+  showPerson(currentItem);
 });
